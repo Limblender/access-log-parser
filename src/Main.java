d71ab07b2c60ed91337e2997fa7a93df627e9432
Vyacheslav Erey <slavaerey@gmail.com>
2025-06-10 15:36:39 +0300

    Добавлены ввод двух чисел и вывод вычислений с ними

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,26 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        Scanner scanner = new Scanner(System.in);
+
+        //получили 2 числа
+        System.out.print("Введите первое число: ");
+        int firstNumber = scanner.nextInt();
+
+        System.out.print("Введите второе число: ");
+        int secondNumber = scanner.nextInt();
+
+        //считаем результаты
+        int sum = firstNumber + secondNumber;
+        int difference = firstNumber - secondNumber;
+        int product = firstNumber * secondNumber;
+        double quotient = (double) firstNumber / secondNumber;
+
+        //Вывод
+        System.out.println("Сумма: +" + sum);
+        System.out.println("Разность: " + difference);
+        System.out.println("Произведение: " + product);
+        System.out.println("Частное: " + quotient);
+
     }
 }
